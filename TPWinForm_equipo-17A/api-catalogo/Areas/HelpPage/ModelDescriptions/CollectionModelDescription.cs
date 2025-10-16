da0175321e34a44edc0f958f3fcbe9818b6feabe
natudellasavia <80315756+natudellasavia@users.noreply.github.com>
2025-10-16 00:20:11 -0300

    Creacion proyecto api-catalogo y catalogoController

---
+++ TPWinForm_equipo-17A/api-catalogo/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace api_catalogo.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TPWinForm_equipo-17A/api-catalogo/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace api_catalogo.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
