126dd4e6e37dff01bd8c4f976f46d7814328400f
Axel29071 <Batistuta622@gmail.com>
2025-10-17 00:11:44 -0300

    Creacion y desarrollo de claseAgregarImagenesDTO

---
+++ TPWinForm_equipo-17A/api-catalogo/Models/AgregarImagenesDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace api_catalogo.Models
+{
+    public class AgregarImagenesDTO
+    {
+        public int IdArticulo { get; set; }
+        public List<string> Imagenes { get; set;  }
+    }
+}
